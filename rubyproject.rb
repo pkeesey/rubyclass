d2405f4262bae6dc009e8c90b8fa8b0749e63cd5
Petet <petet@petet-VirtualBox.(none)>
2014-10-25 14:43:04 -0400

    Commit I just made

--- rubyproject.rb
+++ rubyproject.rb
@@ -1,6 +1,21 @@
-x = [1,2,3]
 
-y = x.map{|z| z * 2}
+# From class on 10-22-2014
+def foo(input)
+	temp = input.reject {|z| z > 10}
+		.reject {|z| z < 5}
+		.map {|z| z * z}
+
+	retval = []
+	temp.reverse_each do |z|
+	puts z
+end
+
+	return retval
+end
+
+
+x = [8,6,7,5,3,0,9]
 
 puts x.inspect
-puts y.inspect
+foo(x)
+
